83b0d57abb10b9092c7658c550be551400d10e71
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-09-02 07:44:31 -0300

    add queue methods

--- src/queues/queue.js
+++ src/queues/queue.js
@@ -3,13 +3,32 @@ class Queue {
     this.data = new Map();
     this.head = 0;
     this.rear = 0;
-    this.length = 0;
   }
 
-  get length() {}
-  enqueue() {}
-  dequeue() {}
-  peek() {}
+  get length() {
+    return this.data.size;
+  }
+
+  enqueue(item) {
+    this.data.set(this.rear, item);
+
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.data.get(this.head);
+    this.data.delete(this.head);
+
+    this.head++;
+
+    return item;
+  }
+
+  peek(index) {
+    if (index === -1) return this.data.get(this.rear - 1);
+
+    return this.data.get(this.head);
+  }
 }
 
 export { Queue };
