e9a29f571aa9c81ae23b1386a0934f4658649622
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-09-02 07:33:58 -0300

    add queue structure and tests

---
+++ src/queues/queue.js
@@ -0,0 +1,15 @@
+class Queue {
+  constructor() {
+    this.data = new Map();
+    this.head = 0;
+    this.rear = 0;
+    this.length = 0;
+  }
+
+  get length() {}
+  enqueue() {}
+  dequeue() {}
+  peek() {}
+}
+
+export { Queue };
---
+++ src/queues/queue.test.js
@@ -0,0 +1,69 @@
+import assert from 'assert';
+import { describe, it } from 'mocha';
+import { Queue } from './queue.js';
+
+describe('Queue test suite', () => {
+    it('#enqueue -> should add a item to the end of the queue', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+
+        assert.deepStrictEqual(queue.data.get(0), 1)
+        assert.deepStrictEqual(queue.data.get(1), 2)
+    })
+
+    it('#dequeue -> should remove the first item of the queue', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+
+        const firstItem = queue.dequeue()
+        const secondItem = queue.dequeue()
+
+        assert.deepStrictEqual(firstItem, 1)
+        assert.deepStrictEqual(secondItem, 2)
+    })
+
+    it('#peek -> should return the first item of the queue without deleting it', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+
+        const firstItem = queue.peek()
+
+        assert.deepStrictEqual(firstItem, 1)
+        assert.deepStrictEqual(queue.dequeue(), firstItem)
+    })
+
+    it('#peek -> should return the last item of the queue if argument is -1', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+
+        const secondItem = queue.peek(-1)
+
+        queue.dequeue()
+
+        assert.deepStrictEqual(secondItem, 2)
+        assert.deepStrictEqual(queue.dequeue(), secondItem)
+    })
+
+    it('#length -> should return the amount of items on the queue', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        assert.deepStrictEqual(queue.length, 3)
+        
+        queue.dequeue()
+        queue.dequeue()
+
+        assert.deepStrictEqual(queue.length, 1)
+    })
+});
